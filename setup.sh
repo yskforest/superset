ab18f939742efe3b257d69c9888e89ee0c2ff8cc
korver58 <ktokyoforest@yahoo.co.jp>
2024-12-17 19:58:01 +0900

    update

--- setup.sh
+++ setup.sh
@@ -3,12 +3,23 @@
 SCTIPR_DIR=$(cd $(dirname $0);pwd)
 cd ${SCTIPR_DIR}
 
-mkdir superset_home
-chmod -R 755 ./superset_home
-chown -R 1000:1000 ./superset_home
+mkdir -p docker_volumes/superset_home
+mkdir -p docker_volumes/postgres_data
+chmod -R 755 ./docker_volumes
+chown -R 1000:1000 ./docker_volumes
 
-mkdir postgres_data
-chmod -R 700 ./postgres_data
-chown -R 999:999 ./postgres_data
+docker compose up -d
+# 初期化
+docker compose exec superset superset db upgrade
+docker compose exec superset superset init
+docker compose exec superset superset fab create-admin \
+  --username admin \
+  --firstname Admin \
+  --lastname User \
+  --email admin@example.com \
+  --password admin
+docker compose down
 
-
+# mkdir postgres_data
+# chmod -R 700 ./postgres_data
+# chown -R 999:999 ./postgres_data
